81a1f1808294dd77f2f8c8c2c5685ad9c8a6e498
lauralani <dev@lauka.space>
2021-05-19 15:19:30 +0200

    added apdconfig

--- eink.py
+++ eink.py
@@ -3,7 +3,7 @@
 import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__))), 'lib')
 
 print(libdir)
 
